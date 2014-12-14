7561772423ed0cc15dfc074e354fa299599ab83d
motoy3d <motoy3d@gmail.com>
2014-12-15 04:08:58 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'gamba'
+    ,hashtag: 'gamba'
+    ,teamName: 'G大阪'
+    ,appName: 'スマートJ for ガンバ大阪'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id921999025?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.gamba'
+    ,color: '#0000bf'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.gamba'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=gamba&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=gamba"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 238685
+    ,nendApiKeyIPhoneIcon: '0442f94dae5000f8223b6578dcbcce9a51c05831'
+    ,nendSpotIdIPhoneBanner: 238684
+    ,nendApiKeyIPhoneBanner: '84242099c72429a82e0c162978ad995c1997a03a'
+    //nend(Android)
+    ,nendSpotIdAndroid: 238682
+    ,nendApiKeyAndroid: 'cb7c7f1af7940fa9a23d80e1da13a2e73ea5de75'
+};
